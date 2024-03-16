7dddde1c34648e6912e78f5389cf50a06d2daf1c
asif <asifsalim0000@gmail.com>
2024-03-16 12:06:24 +0530

    maintenance

--- server/database/connection.js
+++ server/database/connection.js
@@ -2,7 +2,7 @@ const mongoose =require('mongoose');
 
 const connectDB =async()=>{
     try{
-        const con= await mongoose.connect("mongodb://localhost:27017/LOOM",{ 
+        const con= await mongoose.connect("mongodb+srv://asifsalim0000:ljxSl8q2tBGWgCxQ@loom.pfkghlo.mongodb.net/loom?retryWrites=true&w=majority",{ 
         })
         console.log(`MongoDB connected: ${con.connection.host}`);
     }catch(err){
